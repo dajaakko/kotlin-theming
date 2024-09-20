e7a46f382f5f78d2fcfa6f45ebbdc6e01afd031d
dajaakko <jaakko.kaurala@hotmail.fi>
2024-09-20 10:46:32 +0300

    ready theming

---
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.theming.ui.theme.ThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThemingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    myApp(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ThemingTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun myApp(modifier: Modifier = Modifier) {
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text="My title",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = appModifier
+        )
+
+        OutlinedTextField(
+            value="",
+            onValueChange = { },
+            modifier = appModifier
+        )
+
+        Button (
+            onClick ={},
+            modifier = appModifier
+        )  {
+            Text("submit")
+        }
+    }
+}
